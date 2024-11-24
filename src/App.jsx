f51c8c3d4d4ef146e03810031e3dc5f946f94ba7
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-24 15:50:43 -0500

    Navbar + Audio Implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 
 const App = () => {
   return (
     <main className="max-width">
 
+      <Navbar />
       <Hero />
       <About />
 
