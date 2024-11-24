c5b033feab87a577531b5c8a0a8e38df53f96822
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-11-24 14:57:08 -0500

    Animated Title

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,61 +1,61 @@
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
+
 import AnimatedTitle from "./AnimatedTitle";
 
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 const About = () => {
-    useGSAP(() => {
-        const clipAnimation = gsap.timeline({
-            scrollTrigger: {
-                trigger: '#clip',
-                start: 'center center',
-                end: '+=800 center',
-                scrub: 0.5,
-                pin: true,
-                pinSpacing: true,
-            },
-        });
-
-        clipAnimation.to('.mask-clip-path', {
-            width: '100vw',
-            height: '100vh',
-            borderRadius: 0,
-        });
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
     });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
 
   return (
     <div id="about" className="min-h-screen w-screen">
-        <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
-            <h2 className="font-general text-sm uppercase md:text-[10px]">
-                Welcome to Zeniverse
-            </h2>
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[15px]">
+          Welcome to Zeniverse
+        </p>
 
-            <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
 
-            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
-                Disc<b>o</b>ver the world&apos;s <br /> largest shared <b>a</b>dventure
-            </div>
-
-            <div className="about-subtext">
-                <p>The Game of games begins-your life, now an epic MMORPG</p>
-
-                <p>Zeniverse unites every player from countless games and platforms</p>
-            </div>
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zeniverse unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
         </div>
+      </div>
 
-        <div className="h-dvh w-screen" id="clip">
-            <div className="mask-clip-path about-image">
-                <img 
-                    src="img/about.webp"
-                    alt="Background"
-                    className="absolute left-0 top-0 size-full object-cover"
-                />
-            </div>
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
         </div>
+      </div>
     </div>
   );
 };
