827da22e541d4ef0c5a6138f32bb6a3808e4cdcc
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-12-02 14:49:23 -0500

    Updated Video Path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -86,7 +86,7 @@ const Hero = () => {
         });
       });
 
-    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+    const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
 
   return (
     <div id="home" className="relative h-dvh w-screen overflow-x-hidden">
